b9188a4e795708e66f162d163a2ee82b89d75129
Brandon Comerford <bcomerford88@gmail.com>
2023-08-31 23:05:45 -0500

    reapply after push

---
+++ assets/js/theme/category.js
@@ -0,0 +1,58 @@
+import { hooks } from '@bigcommerce/stencil-utils';
+import CatalogPage from './catalog';
+import compareProducts from './global/compare-products';
+import FacetedSearch from './common/faceted-search';
+import { showCategoryModal } from './global/modal';
+
+export default class Category extends CatalogPage {
+    onReady() {
+        this.initCategoryButton();
+        compareProducts(this.context.urls);
+
+        if ($('#facetedSearch').length > 0) {
+            this.initFacetedSearch();
+        } else {
+            this.onSortBySubmit = this.onSortBySubmit.bind(this);
+            hooks.on('sortBy-submitted', this.onSortBySubmit);
+        }
+    }
+
+    initCategoryButton() {
+        const button = document.querySelector('.mobileCategory--toggleButton');
+        if (button) {
+            button.addEventListener('click', showCategoryModal);
+        }
+    }
+
+    initFacetedSearch() {
+        const $productListingContainer = $('#product-listing-container');
+        const $facetedSearchContainer = $('#faceted-search-container');
+        const productsPerPage = this.context.categoryProductsPerPage;
+        const requestOptions = {
+            config: {
+                category: {
+                    shop_by_price: true,
+                    products: {
+                        limit: productsPerPage,
+                    },
+                },
+            },
+            template: {
+                productListing: 'category/product-listing',
+                sidebar: 'category/sidebar',
+            },
+            showMore: 'category/show-more',
+        };
+
+        this.facetedSearch = new FacetedSearch(requestOptions, (content) => {
+            $productListingContainer.html(content.productListing);
+            $facetedSearchContainer.html(content.sidebar);
+
+            $('body').triggerHandler('compareReset');
+
+            $('html, body').animate({
+                scrollTop: 0,
+            }, 100);
+        });
+    }
+}
